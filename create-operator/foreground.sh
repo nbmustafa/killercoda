a8c271c31a7816d8a4e92fd2d7d8f8fdae374575
nashwan <nashwan.bilal@gmail.com>
2025-08-28 13:56:38 +1000

    adding create-operator scenarios

---
+++ create-operator/foreground.sh
@@ -0,0 +1,13 @@
+# download kubebuilder and install locally.
+curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
+chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
+
+# update go
+wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
+go version
+rm go1.21.0.linux-amd64.tar.gz
+
+# make working directory
+mkdir -p src
+cd ./src
